161ce44d1a8d716539322a60bfc88e95e4972a8d
vadimprohorovvv-hub <vadimprohorovvv@gmail.com>
2025-11-02 18:25:48 +0300

    Add files via upload

---
+++ individual.py
@@ -0,0 +1,11 @@
+print("Введите два числа:")
+a = float(input("Первое число: "))
+b = float(input("Второе число: "))
+sum = a + b
+dif = a - b
+div = a / b
+com = a * b
+print(f"сумма: {sum:.2f}")
+print(f"сумма: {dif:.2f}")
+print(f"сумма: {div:.2f}")
+print(f"сумма: {com:.2f}")
